977e434bb5b6aa10fc9a0e66e1926819a3f17680
Matthew Marquis <matthew_marquis@btinternet.com>
2023-10-02 16:09:39 +0100

    Added 'searchBran' GET request route

--- src/main/scala/http/routes/TestRoutes.scala
+++ src/main/scala/http/routes/TestRoutes.scala
@@ -3,27 +3,44 @@ package http.routes
 import cats.effect.Concurrent
 import cats.{Monad, MonadThrow}
 import services.TestService.{TestRoutesAlgebra, TestRoutesInterpreter}
-import cats._
 import cats.effect._
 import cats.implicits._
-import domain.Test._
+import domain.Brand._
+import domain.ABTest._
 import org.http4s.circe._
 import org.http4s._
 import io.circe.generic.auto._
 import io.circe.syntax._
-import org.http4s.headers._
+import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
+import io.circe.{Encoder, Json}
 import org.http4s.dsl._
-import org.http4s.dsl.impl._
 import org.http4s.implicits._
 import org.http4s.server._
 import org.http4s.server.blaze.BlazeServerBuilder
-
+import org.http4s.dsl.impl.QueryParamDecoderMatcher
 
 object TestRoutes extends IOApp {
-  final def testRoutes[F[_] : Monad : MonadThrow : Concurrent](algebra: TestRoutesAlgebra[F]): HttpRoutes[F] = {
+
+  object BrandQueryParamMatcher extends QueryParamDecoderMatcher[String]("brand")
+
+  def testRoutes[F[_] : Monad : MonadThrow : Concurrent](algebra: TestRoutesAlgebra[F]): HttpRoutes[F] = {
     val dsl = Http4sDsl[F]
     import dsl._
-    implicit val newTestEntityDecoder: EntityDecoder[F, NewTest] = CirceEntityDecoder.circeEntityDecoder
+    implicit val testStatusEncoder: Encoder[TestStatus] = new Encoder[TestStatus] {
+      override def apply(status: TestStatus): Json = status match {
+        case Pending => Json.fromString("Pending")
+        case Live => Json.fromString("Live")
+        case Paused => Json.fromString("Paused")
+        case Ended => Json.fromString("Ended")
+      }
+    }
+    implicit val testUpdateEncoder: Encoder[TestUpdate] = new Encoder[TestUpdate] {
+      override def apply(update: TestUpdate): Json = update match {
+        case NoUpdate => Json.fromString("NoUpdate")
+        case TestNameUpdated => Json.fromString("TestNameUpdated")
+        case BrandNameUpdated => Json.fromString("BrandNameUpdated")
+      }
+    }
 
     HttpRoutes.of[F] {
       case req@POST -> Root / "submit" =>
@@ -38,6 +55,12 @@ object TestRoutes extends IOApp {
               Created(s"Test successfully submitted. Execution pending...")
             }
           }
+        }
+
+      case GET -> Root / "searchBrand" :? BrandQueryParamMatcher(brand) =>
+        algebra.getTestsByBrand(BrandName(brand)).flatMap {
+          case Some(brandTests) => Ok(brandTests.asJson)
+          case _ => NotFound(s"'$brand' currently has no tests in the database")
         }
     }
   }
