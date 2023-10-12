244b4ffb93ac3c8916082be9d347a5aaa01ec55a
Matthew Marquis <matthew_marquis@btinternet.com>
2023-10-12 13:19:40 +0100

    Revised submit and searchId request logics in line with interpreter updates

--- src/main/scala/http/routes/TestRoutes.scala
+++ src/main/scala/http/routes/TestRoutes.scala
@@ -1,29 +1,32 @@
 package http.routes
 
+import domain.ABTest._
+import domain.Brand.BrandName
+import services.TestService._
+
 import cats.data.Validated
 import cats.effect.Concurrent
-import cats.{Applicative, ApplicativeError, ApplicativeThrow, Monad, MonadThrow}
+import cats.{Applicative, ApplicativeThrow, Monad, MonadThrow}
 import services.TestService.{TestRoutesAlgebra, TestRoutesInterpreter}
 import cats.effect._
 import cats.implicits._
-import domain.ABTest._
-import domain.Brand.BrandName
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.{W, api, refineV}
+import com.datastax.oss.driver.api.core.cql.ResultSet
+import com.datastax.oss.driver.api.core.data.UdtValue
+import eu.timepit.refined.{W, refineV}
 import eu.timepit.refined.string.MatchesRegex
 import org.http4s.circe._
 import org.http4s._
 import io.circe.generic.auto._
 import io.circe.syntax._
-import io.circe.{Decoder, Encoder, Json}
-import io.circe.generic.semiauto.deriveDecoder
+import io.circe.{Encoder, Json}
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl._
 import org.http4s.implicits._
 import org.http4s.server._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.dsl.impl.{OptionalValidatingQueryParamDecoderMatcher, QueryParamDecoderMatcher}
+import scala.jdk.CollectionConverters._
 
-import java.nio.charset.MalformedInputException
 import java.util.UUID
 import scala.util.Try
 
@@ -35,7 +38,7 @@ object TestRoutes extends IOApp {
   private object TestIdQueryParamMatcher extends OptionalValidatingQueryParamDecoderMatcher[UUID]("testId")
   private object BrandQueryParamMatcher extends QueryParamDecoderMatcher[String]("brand")
 
-  private final def testRoutes[F[_] : Monad : MonadThrow : Concurrent](algebra: TestRoutesAlgebra[F]): HttpRoutes[F] = {
+  def testRoutes[F[_] : Monad : MonadThrow : Concurrent](algebra: TestRoutesAlgebra[F]): HttpRoutes[F] = {
     val dsl = Http4sDsl[F]
     import dsl._
 
@@ -57,35 +60,55 @@ object TestRoutes extends IOApp {
     }
 
     // Example POST runtime validation
-    def isValid(text: String): Validated[String, String] =
-      if (text.matches("^[a-zA-Z0-9\\s,.!?']+")) {
+    def nameIsValid(text: String): Validated[String, String] =
+      if (text.matches("^[a-zA-Z0-9\\s,.&']+") && text.length <= 30) {
         Validated.valid(text)
       }
-      else Validated.invalid(s"Invalid format for: $text. Please use only letters, numbers or the special characters ,.!?';")
+      else Validated.invalid(s"Invalid format for: $text. Please use only letters, numbers or the special characters ,.&'. Character limit is 30.;")
+
+    def adIsValid(text: String): Validated[String, String] =
+      if (text.matches("^[a-zA-Z0-9\\s,.!?&']+") && text.length <= 120) {
+        Validated.valid(text)
+      }
+      else Validated.invalid(s"Invalid format for: $text. Please use only letters, numbers or the special characters ,.!?'. Character limit is 120.;")
 
     implicit val newTestEntityDecoder: EntityDecoder[F, NewTest] = jsonOf[F, NewTest].flatMapR { newTest =>
-      (isValid(newTest.brandName.brandName),
-        isValid(newTest.testName.testName),
-        newTest.adTextVariants.map(text => isValid(text)).sequence
-      ).mapN { (_, _, _) => newTest // to simply return newTest if validation passed
+      (nameIsValid(newTest.brandName.brandName),
+        nameIsValid(newTest.testName.testName),
+        newTest.adTextVariants.map(text => adIsValid(text)).sequence
+      ).mapN { (_, _, _) => newTest
       }.fold (
         errors => DecodeResult.failure(ApplicativeThrow[F].raiseError(MalformedMessageBodyFailure(errors.split(";").mkString("\n")))),
         result => DecodeResult.success(Applicative[F].pure(result))
       )
     }
 
+    implicit val resultSetEntityEncoder: EntityEncoder[F, ResultSet] =
+      EntityEncoder[F, String].contramap { resultSet =>
+        val jsonResult = resultSet.all().asScala.map { row =>
+          Map(
+            "testId" -> row.getUuid("testid"),
+            "brandName" -> row.getString("brandname"),
+            "testName" -> row.getString("testname"),
+            "adVariants" -> row.getList("advariants", classOf[UdtValue]).asScala.toList,
+            "testSpend" -> row.getBigDecimal("testspend"),
+            "testDuration" -> row.getDouble("testduration"),
+            "testSubmissionDate" -> row.getInstant("testsubmissiondate"),
+            "testStartDate" -> row.getInstant("teststartdate"),
+            "testStatus" -> row.getString("teststatus"),
+            "testUpdate" -> row.getString("testupdate")
+          ).toString
+        }
+        jsonResult.asJson.spaces2
+      }
+
     HttpRoutes.of[F] {
       case req@POST -> Root / "submit" =>
         req.as[NewTest].flatMap { newTest =>
           val response = algebra.addTest(newTest)
-          val isEmptyF: F[Boolean] = response.map(testArray => testArray.isEmpty)
-
-          isEmptyF.flatMap { isEmpty =>
-            if (isEmpty) {
-              InternalServerError("Database connection failed. Please try again.")
-            } else {
-              Ok(s"Test successfully submitted. Execution pending...")
-            }
+          response.flatMap {
+            case AddTestQuerySuccess => Ok("Test successfully submitted. Execution pending...")
+            case AddTestQueryError  => InternalServerError("Database communication error. If this problem persists please contact the database administrator.")
           }
         }.recoverWith {
           case MalformedMessageBodyFailure(errorMessage, _) =>
@@ -103,7 +126,6 @@ object TestRoutes extends IOApp {
             BadRequest("Invalid brand name format. Only letters and numbers allowed.")
         }
 
-
       case GET -> Root / "searchId" :? TestIdQueryParamMatcher(testId) =>
         testId match {
           case Some(validatedUUID) =>
@@ -111,8 +133,9 @@ object TestRoutes extends IOApp {
               _ => BadRequest("Invalid UUID format for test ID"),
               uuid => {
                 algebra.getTestById(TestId(uuid)).flatMap {
-                  case Some(test) => Ok(test.asJson)
-                  case _ => NotFound(s"No tests found for the id $uuid")
+                  case Right(resultSet) => Ok(resultSet)
+                  case Left(TestIdNotFoundError) => BadRequest(s"No records found for test id: $uuid")
+                  case Left(DatabaseConnectionError) => InternalServerError("Database communication error. If this problem persists please contact the database administrator.")
                 }
               }
             )
